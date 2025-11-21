7ac57f63bb4399f41ef92cebd4d5d7e2843b3526
Nadjib MOUSSAOUI <nadjibmoussaoui@outlook.com>
2025-11-21 10:05:37 +0100

    repondre au 4 question vers la BD

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from questions_vers_la_BD import tester_contrat_parking, tester_borne_parking
-
+from questions_vers_la_BD import tester_contrat_parking, tester_borne_parking, extraire_nombre_vehicules_par_client,extraire_places_par_parking, extraire_clients_abonnement_long, extraire_paiements_importants
 # 🔹 Exemple d'appel
 if __name__ == "__main__":
     # test Question 1 et 2 : borne et parking
 
-    ## Borne Active et parking reconnue
+    """ ## Borne Active et parking reconnue
     print(tester_borne_parking("PK0001", "B0001"))
     # parking reconnue mais borne en_panne
     print(tester_borne_parking("PK0002", "B0002"))
@@ -22,4 +21,22 @@ if __name__ == "__main__":
     # Parking inexistant (selon les données de la base)
     print(tester_contrat_parking("PK9999", "CT00001"))
     # Les deux inexistants
-    print(tester_contrat_parking("PK9999", "CT99999"))
+    print(tester_contrat_parking("PK9999", "CT99999")) """
+    
+    """  #test Question 3 : le nombre de vehicule par client 
+    print(extraire_nombre_vehicules_par_client()) """
+
+    #test Question 4 : le nombre de place libre et occupé dans un parking
+    """ resultat = extraire_places_par_parking()
+        faut tester si le resultat est non null
+    for ligne in resultat.split("\n"):
+        nom_parking, places_libres, places_occupees = ligne.split()
+        print(f"Parking: {nom_parking} | Places libres: {places_libres} | Places occupées: {places_occupees}")
+
+ """
+        #test Question 5 :  les informations des clients qui possede un abonnement de minimum de 6 mois
+
+""" print(extraire_clients_abonnement_long()) """
+
+    #test Question 6 : les plus grands tarifs payé par des clients pas moins de 100 euro
+print(extraire_paiements_importants())
